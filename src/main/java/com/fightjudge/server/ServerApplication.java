bbde7097210e700cc0e8805a8f80603e885d4682
Vadym-Lopatka <mirve09021990lik>
2019-03-11 17:25:20 +0200

    added Kafka Streams

--- src/main/java/com/fightjudge/server/ServerApplication.java
+++ src/main/java/com/fightjudge/server/ServerApplication.java
@@ -3,11 +3,13 @@ package com.fightjudge.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
